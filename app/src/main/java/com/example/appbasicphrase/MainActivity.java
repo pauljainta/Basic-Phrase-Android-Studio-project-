d6fe540e73d99c816d88f9b6fd836d4175131905
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-11 01:38:39 +0600

    Complete

--- app/src/main/java/com/example/appbasicphrase/MainActivity.java
+++ app/src/main/java/com/example/appbasicphrase/MainActivity.java
@@ -2,10 +2,61 @@ package com.example.appbasicphrase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    public void Play(View view)
+    {
+        if(view.getTag().toString().equalsIgnoreCase("areyoudone"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.areyoudone);
 
+        }
+        else if(view.getTag().toString().equalsIgnoreCase("areyoumad"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.areyoumad);
+
+        }
+        else if(view.getTag().toString().equalsIgnoreCase("goodjob"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.goodjob);
+
+        }
+        else if(view.getTag().toString().equalsIgnoreCase("goodluck"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.goodluck);
+
+        }
+       else if(view.getTag().toString().equalsIgnoreCase("goodmorning"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.goodmorning);
+
+        }
+
+        else if(view.getTag().toString().equalsIgnoreCase("hello"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.hello);
+
+        }
+
+        else if(view.getTag().toString().equalsIgnoreCase("howareyou"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.howareyou);
+
+        }
+
+        else if(view.getTag().toString().equalsIgnoreCase("welcome"))
+        {
+            mediaPlayer=MediaPlayer.create(this,R.raw.welcome);
+
+        }
+
+        mediaPlayer.start();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
